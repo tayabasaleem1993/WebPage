7857068f0c538249ac9e6b45775d075bb52c145b
tayabasaleem1993 <119078937+tayabasaleem1993@users.noreply.github.com>
2022-11-28 13:37:20 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
-require([
-  "esri/config",
-  "esri/Map", 
-  "esri/views/MapView"
-  ], funtion (esriConfig, Map, MapView) {
-        //Setting the API Key 
-        esriConfig.apiKey = "AAPK78146bcb95be4b62ab610fa11305a227m2MYYbA3ylOfcGRmNFxDIq0EPukzUxpkF0xN3k3x54GHVtlvRRHNKpdCmAF9iqJs";
-        
-        const map = new Map({ 
-        basemap: "arcgis-topographic"
+require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
+        /************************************************************
+         * Creates a new WebMap instance. A WebMap must reference
+         * a PortalItem ID that represents a WebMap saved to
+         * arcgis.com or an on-premise portal.
+         *
+         * To load a WebMap from an on-premise portal, set the portal
+         * url with esriConfig.portalUrl.
+         ************************************************************/
+        const webmap = new WebMap({
+          portalItem: {
+            // autocasts as new PortalItem()
+            id: "a57250d372094bd694dc49966b64e276"
+          }
         });
-        
-      const view = new MapView({
-        map: map,
-        center: [-118.805, 34.027],
-        zoom: 13, // scale: 72223.819286
-        container: "viewDiv",
-        constraints: {
-          snapToZoom: false
-        }
-      });
-});
-          
+
+        /************************************************************
+         * Set the WebMap instance to the map property in a MapView.
+         ************************************************************/
+        const view = new MapView({
+          map: webmap,
+          container: "viewDiv"
+        });
+      });  
