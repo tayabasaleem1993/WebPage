fd492a3e8c20a12d5d8faaa62f68c34aa4c6dd23
tayabasaleem1993 <119078937+tayabasaleem1993@users.noreply.github.com>
2022-11-28 11:36:50 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ require([
   "esri/views/MapView"
   ], funtion (esriConfig, Map, MapView) {
         //Setting the API Key 
-        esriConfig.apiKey = "AAPK78146bcb95be4b62ab610fa11305a227m2MYYbA3ylOfcGRmNFxDIq0EPukzUxpkF0xN3k3x54GHVtlvRRHNKpdCmAF9iqJs"
+        esriConfig.apiKey = "AAPK78146bcb95be4b62ab610fa11305a227m2MYYbA3ylOfcGRmNFxDIq0EPukzUxpkF0xN3k3x54GHVtlvRRHNKpdCmAF9iqJs";
         
         const map = new Map({ 
         basemap: "arcgis-dark-gray" 
