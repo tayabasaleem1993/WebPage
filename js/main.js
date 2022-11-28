3e09577f12ffd2e3292a66d400063f7af14782f3
tayabasaleem1993 <119078937+tayabasaleem1993@users.noreply.github.com>
2022-11-28 11:59:21 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,17 +4,20 @@ require([
   "esri/views/MapView"
   ], funtion (esriConfig, Map, MapView) {
         //Setting the API Key 
-        esriConfig.apiKey = "AAPK78146bcb95be4b62ab610fa11305a227m2MYYbA3ylOfcGRmNFxDIq0EPukzUxpkF0xN3k3x54GHVtlvRRHNKpdCmAF9iqJs"
+        esriConfig.apiKey = "AAPK78146bcb95be4b62ab610fa11305a227m2MYYbA3ylOfcGRmNFxDIq0EPukzUxpkF0xN3k3x54GHVtlvRRHNKpdCmAF9iqJs";
         
         const map = new Map({ 
-        basemap: "arcgis-dark-gray" 
+        basemap: "arcgis-topographic"
         });
         
-        const view = new MapView({
-          container: "viewDiv", 
-          map: map, 
-          extent: [32.7767, 96.7970], 
-          zoom: 12
-        });
+      const view = new MapView({
+        map: map,
+        center: [-118.805, 34.027],
+        zoom: 13, // scale: 72223.819286
+        container: "viewDiv",
+        constraints: {
+          snapToZoom: false
+        }
+      });
 });
           
